aab5cda75c7f4e0a54f238c09c82dc72f3078fc1
Nadia Mimoun <mimounnadia205@gmail.com>
2021-09-23 00:50:34 -0500

    add testV2 (100%)

--- src/test/java/edu/miu/service/FunctionalServiceTest.java
+++ src/test/java/edu/miu/service/FunctionalServiceTest.java
@@ -1,15 +1,17 @@
 package edu.miu.service;
 
 import edu.miu.model.*;
+import edu.miu.utils.Tuple;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 public class FunctionalServiceTest {
@@ -63,10 +65,23 @@ public class FunctionalServiceTest {
 
 
         movie1 = new Movie(2300l, genres, "homepage1", 1L, keywords, "english", "title1", "overView1", 12.987, productionCompanies, productionCountries, LocalDate.of(2010, 12, 3), 278796L, 162.98, spokenLanguages, Status.Released, "tagline1", "title1", 7.2, 11800);
-        movie2 = new Movie(24500l, genres, "homepage2", 2L, keywords, "frensh", "title2", "overView2", 65.987, productionCompanies, productionCountries, LocalDate.of(2010, 04, 15), 98765L, 543.976, spokenLanguages, Status.Released, "tagline2", "title2", 5.2, 9800);
-        movie3 = new Movie(65432l, genres, "homepage3", 3L, keywords, "english", "title3", "overView3", 87.8765, productionCompanies, productionCountries, LocalDate.of(2010, 6, 15), 87653L, 987.654, spokenLanguages, Status.Released, "tagline3", "title3", 8.2, 12500);
+        movie2 = new Movie(24500l, List.of(genre2), "homepage2", 2L, keywords, "frensh", "title2", "overView2", 65.987, productionCompanies, productionCountries, LocalDate.of(2010, 04, 15), 98765L, 543.976, spokenLanguages, Status.Released, "tagline2", "title2", 5.2, 9800);
+        movie3 = new Movie(65432l, List.of(genre3), "homepage3", 3L, keywords, "english", "title3", "overView3", 87.8765, productionCompanies, productionCountries, LocalDate.of(2010, 6, 15), 87653L, 987.654, spokenLanguages, Status.Released, "tagline3", "title3", 8.2, 12500);
         movie4 = new Movie(98765l, genres, "homepage4", 4L, keywords, "frensh", "title4", "overView4", 45.99867, productionCompanies, productionCountries, LocalDate.of(2010, 8, 30), 122786L, 3564.976, spokenLanguages, Status.Released, "tagline4", "title4", 6.4, 6780);
         movies = List.of(movie1, movie2, movie3, movie4);
+
+    }
+    @Test
+    public void totalBenefit() {
+
+        assertTrue(FunctionalService.totalBenefit.apply(movie1)==276496);
+
+    }
+    @Test
+    public void isReleaseYearSame() {
+
+        assertTrue(FunctionalService.isReleaseYearSame.test(movie1,2010L));
+        assertFalse(FunctionalService.isReleaseYearSame.test(movie1,2004L));
 
     }
 
@@ -85,6 +100,94 @@ public class FunctionalServiceTest {
         assertTrue(FunctionalService.topKSuccessfulMovies.apply(movies, 2015L, 3L).isEmpty());
 
     }
+
+    @Test
+    public void movieGenrePairGenerator () {
+        assertEquals(FunctionalService.movieGenrePairGenerator .apply(movie1,genres).size(), 3);
+
+        assertEquals(FunctionalService.movieGenrePairGenerator.apply(movie1,genres).get(0).getValue(), movie1);
+        assertEquals(FunctionalService.movieGenrePairGenerator.apply(movie1,genres).get(0).getKey(), genre1);
+
+
+    }
+
+    @Test
+    public void  sortListOfMovieByAverageRating () {
+        ImmutablePair pair=new ImmutablePair(genre2,List.of(movie1,movie2,movie4));
+        assertEquals(FunctionalService. sortListOfMovieByAverageRating .apply(pair,2L).size(), 2);
+        assertEquals(FunctionalService. sortListOfMovieByAverageRating .apply(pair,2L), List.of(movie1,movie4));
+
+    }
+    @Test
+    public void  convertListOfMoviesToPairOfNameAndRating () {
+        assertEquals(FunctionalService. convertListOfMoviesToPairOfNameAndRating .apply(movies).size(), 4);
+        assertEquals(FunctionalService. convertListOfMoviesToPairOfNameAndRating .apply(movies), List.of("title1(7.2)", "title2(5.2)", "title3(8.2)", "title4(6.4)"));
+
+    }
+    @Test
+    public void  topKMoviesByGenre () {
+
+        assertEquals(FunctionalService. topKMoviesByGenre .apply(movies,2010L,2L).size(), 3);
+        assertEquals(FunctionalService. topKMoviesByGenre .apply(movies,2010L,2L).get("genre3"), List.of("title3(8.2)", "title1(7.2)"));
+
+    }
+
+    @Test
+    public void  totalRevenue () {
+        assertTrue(FunctionalService. totalRevenue .apply(movies)==588000L);
+    }
+    @Test
+
+    public void  companyMoviesPair () {
+        assertEquals(FunctionalService. companyMoviesPair .apply(productionCompanies,movie1).size(),4);
+        assertEquals(FunctionalService. companyMoviesPair .apply(productionCompanies,movie1).get(0).getKey(),pc1);
+
+    }
+
+    @Test
+
+    public void  mostSuccessfulKProductionCompany() {
+        assertEquals(FunctionalService. mostSuccessfulKProductionCompany .apply(movies,2010L,2L).size(),2);
+        assertEquals(FunctionalService. mostSuccessfulKProductionCompany .apply(movies,2010L,2L).get(0).getKey(),"company1");
+
+    }
+
+    @Test
+
+    public void  mostPopularMovie() {
+        assertTrue(FunctionalService. mostPopularMovie .apply(movies).isPresent());
+        assertEquals(FunctionalService. mostPopularMovie .apply(movies).get(),movie3);
+
+    }
+
+    @Test
+
+    public void   kMostPopularMovieInAGivenYear() {
+        assertEquals(FunctionalService.  kMostPopularMovieInAGivenYear .apply(movies,2010L).size(),3);
+        assertEquals(FunctionalService.  kMostPopularMovieInAGivenYear .apply(movies,2010L),List.of("genre3 -> title3 [87.8765]", "genre1 -> title4 [45.99867]", "genre2 -> title2 [65.987]"));
+
+    }
+
+    @Test
+
+    public void    movieSpokenLanguagePairGenerator() {
+        assertEquals(FunctionalService.   movieSpokenLanguagePairGenerator .apply(movie1,spokenLanguages).size(),3);
+        assertEquals(FunctionalService.   movieSpokenLanguagePairGenerator .apply(movie1,spokenLanguages).get(0).getKey(),language1);
+
+    }
+    @Test
+
+    public void    movieCountByLanguageInGivenYear() {
+        assertEquals(FunctionalService.   movieCountByLanguageInGivenYear .apply(movies,2010L).size(),3);
+        assertEquals(FunctionalService.   movieCountByLanguageInGivenYear .apply(movies,2010L).get(0).getKey(),"iso2");
+        assertTrue(FunctionalService.   movieCountByLanguageInGivenYear .apply(movies,2010L).get(0).getValue()==4);
+
+
+    }
+
+
+
+
 
 
 }
